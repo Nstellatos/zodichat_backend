4e7020ade5ebfe1839f1a1072de488221d4f2912
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2021-11-24 20:51:52 -0500

    users table

--- db/migrate/20211125002819_create_users.rb
+++ db/migrate/20211125002819_create_users.rb
@@ -1,6 +1,14 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
+      t.string :username
+      t.string :password_digest
+      t.string :sun_sign 
+      t.string :moon_sign 
+      t.string :rising_sign 
+      t.text :bio 
+
+
 
       t.timestamps
     end
