30567a6dcdb911aa87be94794e557b874ab46084
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2021-11-26 18:08:30 -0500

    seed db w/ a test post

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Post.create(title: 'Taurus Reading', description: 'this is a test post', image_url: 'https://www.datocms-assets.com/7756/1600770888-zodiac-sign-4374405.jpg?auto=enhance%2Cformat&h=630&w=1200')
