5ecc06c41390d66b81969df1ee0d363ac78d5163
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2021-11-26 17:45:41 -0500

    added Post resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_25_002819) do
+ActiveRecord::Schema.define(version: 2021_11_26_223403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
- 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "image_url"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -28,5 +40,13 @@ ActiveRecord::Schema.define(version: 2021_11_25_002819) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  
+  create_table "videos", force: :cascade do |t|
+    t.string "video_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "category_id", null: false
+    t.index ["category_id"], name: "index_videos_on_category_id"
+  end
+
+  add_foreign_key "videos", "categories"
 end
