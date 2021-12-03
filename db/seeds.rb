5b0a02fd1ee3c064251efe67438fd1f64e78a75b
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2021-12-03 18:27:52 -0500

    seed db for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Post.create(title: 'Taurus Reading', description: 'this is a test post', image_url: 'https://www.datocms-assets.com/7756/1600770888-zodiac-sign-4374405.jpg?auto=enhance%2Cformat&h=630&w=1200')
+
+#Aries, Taurus, Gemini, Cancer, Leo, Virgo, Libra, Scorpio, Sagittarius, Capricorn, Aquarius and Pisces
+aries = Sign.create(name: 'Aries')
+taurus = Sign.create(name: 'Taurus')
+gemini = Sign.create(name: 'Gemini')
+cancer = Sign.create(name: 'Cancer')
+leo = Sign.create(name: 'Leo')
+virgo = Sign.create(name: 'Virgo')
+libra = Sign.create(name: 'Libra')
+scorpio = Sign.create(name: 'Scorpio')
+sagittarius = Sign.create(name: 'Sagittarius') 
+capricorn = Sign.create(name: 'Capricorn')
+aquarius = Sign.create(name: 'Aquarius')
+pisces = Sign.create(name: 'Pisces')
+
+nstella = User.create(username: 'nstella', password_digest: 'password', sun_sign: 'taurus', image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Mr._Smiley_Face.svg/1200px-Mr._Smiley_Face.svg.png', bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh tortor id aliquet lectus proin nibh nisl condimentum. Tellus orci ac auctor augue mauris augue neque.')
+
+Post.create(title: 'Taurus Reading test 1', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', image_url: 'https://www.datocms-assets.com/7756/1600770888-zodiac-sign-4374405.jpg?auto=enhance%2Cformat&h=630&w=1200', sign: taurus, user: nstella)
+
+Post.create(title: 'Taurus Reading test 2', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', image_url: 'https://www.datocms-assets.com/7756/1600770888-zodiac-sign-4374405.jpg?auto=enhance%2Cformat&h=630&w=1200', sign: taurus, user: nstella)
+
+Post.create(title: 'Taurus Reading test 3', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', image_url: 'https://www.datocms-assets.com/7756/1600770888-zodiac-sign-4374405.jpg?auto=enhance%2Cformat&h=630&w=1200', sign: taurus, user: nstella)
+
+Post.create(title: 'Taurus Reading test 4', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', image_url: 'https://www.datocms-assets.com/7756/1600770888-zodiac-sign-4374405.jpg?auto=enhance%2Cformat&h=630&w=1200', sign: taurus, user: nstella)
+
+Post.create(title: 'Taurus Reading test 5', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', image_url: 'https://www.datocms-assets.com/7756/1600770888-zodiac-sign-4374405.jpg?auto=enhance%2Cformat&h=630&w=1200', sign: taurus, user: nstella)
+
+
