a19eb9b3a5316ea3d0aafc1e8bc7a8b8023f8464
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2021-12-14 16:39:58 -0500

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :signs do
-    resources :posts
-  end 
+  resources :signs 
+  resources :posts
+  
+  
+
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
