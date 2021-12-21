55e7623d09a10359d3956dcb5c3fc542a21d7352
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2021-12-21 16:20:16 -0500

    create action for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,15 @@ class UsersController < ApplicationController
     def show 
         user = User.find(params[:id])
     end
+
+    def create 
+        user = User.create!(user_params)
+
+    end
+
+
+    private
+    def user_params
+        params.permit(:username, :password, :sun_sign)
+    end
 end
