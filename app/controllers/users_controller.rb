754832d3cd09949247410b788b74013ae1ecd6a7
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2021-12-30 00:24:57 -0500

    action for user create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,22 @@
 class UsersController < ApplicationController
-    def show 
-        user = User.find(params[:id])
+
+    def index
+        users= User.all 
+        render json: users
     end
 
-    def create 
-        user = User.create!(user_params)
+    def show 
+        user= User.find(params[:id])
         render json: user
-
+    end
+    
+    def create 
+        user = User.new(user_params)
+        if user.save
+            render_user_with_token(user)
+        else
+            render json: {errors: user.errors.full_messages.to_sentence}, status: :unprocessable_entity
+        end
     end
 
 
