57ff16c405024bafb13f827d5ecb4b293d3d0196
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2021-12-30 00:26:20 -0500

    post is created with logged in user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,13 @@
 class PostsController < ApplicationController
-    def index
-        posts = Post.all
-        render json: posts
-    end
-    def show
-        post = Post.find(params[:id])
-        render json: post
-    end
+  
+     def create
+    post = logged_in_user.posts.create!(post_params)
+    render json: post
+  end
+
+  private
+
+  def post_params
+    params.permit(:sign_id, :title, :image_url, :description)
+  end
 end
