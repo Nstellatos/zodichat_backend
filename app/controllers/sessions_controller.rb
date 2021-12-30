6293f60925aec1c186f9ced05779be3f9f416d57
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2021-12-30 00:24:02 -0500

    updated to implement jwt

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,16 @@
 class SessionsController < ApplicationController
 
-    def login
-        user= User.find_by_username(params[:user])
-        if user && user.authenticate(params[:password])
-            render json: user
-        else
-            render json: {errors: "INVALID USERNAME OR PASSWORD"}, status :forbidden
-        end
+   def create
+    user = User.find_by_username(params[:username])
+    if user && user.authenticate(params[:password])
+      render_user_with_token(user)
+    else
+      render json: {errors: "Invalid Username and/or Password, get good!"}, status: :forbidden
     end
+  end
+
+  def autologin
+    render_user_with_token(logged_in_user)
+  end
 
 end
