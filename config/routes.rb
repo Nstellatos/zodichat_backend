8ba75f07384c60b8f1df5ab60eba4d102b7be858
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2021-12-30 00:25:58 -0500

    added routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   resources :signs do
   resources :posts
+
   end
   
   
 
   resources :users
+  resources :sessions, only: [:create]
+  get "/me", to: 'sessions#autologin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
