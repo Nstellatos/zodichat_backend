43d481430fcf1cd9111401ea19e593711fd56216
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2022-01-14 14:50:10 -0500

    added created_at attribute to posts

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :image_url, :username
+  attributes :id, :title, :description, :image_url, :username, :created_at
 
   def username
     self.object.user.username
