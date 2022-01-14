3ddc951795804a0b43232b24ec9cd75ab32a467a
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2022-01-14 14:49:45 -0500

    added created_at attribute to users

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :username, :bio, :image_url, :sun_sign
+  attributes :username, :bio, :image_url, :sun_sign, :id, :created_at
   has_many :posts
+ 
+ 
+
+
 end
