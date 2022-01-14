eba8849f1c39aaee70a8119eea7f9dee697ef5ec
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2022-01-14 14:47:14 -0500

    index & show action for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,15 @@
 class PostsController < ApplicationController
   
+  def index 
+    posts = Post.all 
+    render json: posts
+  end
+def show 
+  post = Post.find(params[:id])
+  render json: post
+end
+
+
      def create
     post = logged_in_user.posts.create!(post_params)
     render json: post
